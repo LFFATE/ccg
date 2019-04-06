0b7fb206815c130c8322ef805607a276857a7baa
Mikhail Shemenev <mshemenev@simtechdev.com>
2019-04-06 19:27:22 +0400

    add getPath tests

---
+++ tests/generators/AddonXmlGeneratorTest.php
@@ -0,0 +1,407 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+use generators\AddonXml\AddonXmlGenerator;
+use generators\AddonXml\exceptions\InvalidContentException;
+use generators\AddonXml\exceptions\DuplicateIdException;
+use generators\XML;
+use filesystem\Filesystem;
+
+/**
+ * @todo remove dependency from filesystem use file_get_contents instead
+ */
+final class AddonXmlGeneratorTest extends TestCase
+{
+    private $generator;
+    private $config;
+    private $testFilename = ROOT_DIR . '/tests/sources/xml/test.xml';
+
+    protected function setUp(): void
+    {
+        $this->config = new Config([
+            'addon.id=sd_addon'
+        ],
+        [
+            'addon.edition_type' => 'ROOT,ULT:VENDOR',
+            'addon.version' => '4.9',
+            'addon.priority' => '665',
+            'addon.status' => 'active',
+            'addon.auto_install' => 'ULTIMATE',
+            'filesystem.output_path_relative' => './'
+        ]);
+        $this->generator = new AddonXmlGenerator($this->config);
+    }
+
+    public function testGetPath(): void
+    {
+        $this->assertSame(
+            get_absolute_path(ROOT_DIR . $this->config->get('filesystem.output_path_relative') . 'app/addons/sd_addon/addon.xml'),
+            $this->generator->getPath()
+        );
+    }
+
+    /**
+     * @covers AddonXmlGenerator::toXml
+     * @covers AddonXmlGenerator::create
+     */
+    public function testCreate(): void
+    {
+        $xmlString = $this->generator->create()->toXml();
+        $simpleXml = new \SimpleXMLElement($xmlString);
+        $addon = new Xml($simpleXml);
+
+        $idElement = $addon->getSingleElement('id');
+        $this->assertEquals(
+            (string) $idElement->getSelf(),
+            'sd_addon'
+        );
+
+        $versionElement = $addon->getSingleElement('version');
+        $this->assertEquals(
+            (string) $versionElement->getSelf(),
+            '4.9'
+        );
+
+        $priorityElement = $addon->getSingleElement('priority');
+        $this->assertEquals(
+            (string) $priorityElement->getSelf(),
+            '665'
+        );
+
+        $statusElement = $addon->getSingleElement('status');
+        $this->assertEquals(
+            (string) $statusElement->getSelf(),
+            'active'
+        );
+
+        $settingsElement = $addon->getSingleElement('settings');
+        $this->assertNotEquals(
+            $settingsElement,
+            null
+        );
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setContent
+     */
+    public function testSetContent(): void
+    {
+        $filesystem = new Filesystem();
+        $this->generator->setContent($filesystem->read($this->testFilename));
+        $result = $this->generator->toXml();
+
+        $this->assertXmlStringEqualsXmlFile(
+            $this->testFilename,
+            $result
+        );
+
+        $this->expectException(InvalidContentException::class);
+        $this->generator->setContent('');
+        $this->expectException(InvalidContentException::class);
+        $this->generator->setContent(13);
+    }
+
+    /**
+     * @covers AddonXmlGenerator::createAddon
+     */
+    public function testCreateAddon(): void
+    {
+        $xml = $this->generator->createAddon()->toString();
+
+        $actual = new DOMDocument;
+        $actual->loadXML($xml);
+
+        $expected = new DOMDocument;
+        $expected->loadXML('<addon></addon>');
+
+        $this->assertEqualXMLStructure(
+            $expected->firstChild,
+            $actual->firstChild,
+            true
+        );
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setScheme
+     */
+    public function testSetScheme(): void
+    {
+        $test_value = '4.0';
+        $xml = $this->generator->createAddon()->setScheme($test_value)->toString();
+        $this->_testAddonAttribute($xml, 'scheme', $test_value);
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setEditionType
+     */
+    public function testSetEditionType(): void
+    {
+        $test_value = 'ULT:VENDOR';
+        $xml = $this->generator->createAddon()->setEditionType($test_value)->toString();
+        $this->_testAddonAttribute($xml, 'edition_type', $test_value);
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setId
+     */
+    public function testSetId(): void
+    {
+        $test_value = 'sd_addon_id';
+        $xml = $this->generator->createAddon()->setId($test_value)->toString();
+        $this->_testAddonNode($xml, 'id', $test_value);
+    }
+
+
+    /**
+     * @covers AddonXmlGenerator::setVersion
+     */
+    public function testSetVersion(): void
+    {
+        $test_value = '4.9.9.8';
+        $xml = $this->generator->createAddon()->setVersion($test_value)->toString();
+        $this->_testAddonNode($xml, 'version', $test_value);
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setPriority
+     */
+    public function testSetPriority(): void
+    {
+        $test_value = '1000';
+        $xml = $this->generator->createAddon()->setPriority($test_value)->toString();
+        $this->_testAddonNode($xml, 'priority', $test_value);
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setStatus
+     */
+    public function testSetStatus(): void
+    {
+        $test_value = 'active';
+        $xml = $this->generator->createAddon()->setStatus($test_value)->toString();
+        $this->_testAddonNode($xml, 'status', $test_value);
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setAutoInstall
+     */
+    public function testSetAutoInstall(): void
+    {
+        $test_value = 'MULTIVENDOR';
+        $xml = $this->generator->createAddon()->setAutoInstall($test_value)->toString();
+        $this->_testAddonNode($xml, 'auto_install', $test_value);
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setSettings
+     * @covers AddonXmlGenerator::getSettings
+     */
+    public function testSetAndGetSettings(): void
+    {
+        $xml = $this->generator->createAddon()->setSettings()->toString();
+        $this->_testAddonNode($xml, 'settings', '');
+
+        $settingsElement = $this->generator->createAddon()->setSettings()->getSettings();
+        $this->assertSame(
+            'settings',
+            $settingsElement->getName()
+        );
+    }
+
+    /**
+     * @covers AddonXmlGenerator::setSettingsLayout
+     */
+    public function testSetSettingsLayout(): void
+    {
+        $xml =
+            $this
+                ->generator
+                ->createAddon()
+                ->setSettings()
+                ->setSettingsLayout('separate')
+                ->toString();
+
+        $actual = new DOMDocument;
+        $actual->loadXML($xml);
+
+        $expected = new DOMDocument;
+        $expected->loadXML('<addon><settings layout="separate"></settings></addon>');
+
+        $this->assertEqualXMLStructure(
+            $expected->firstChild,
+            $actual->firstChild,
+            true
+        );
+    }
+
+    /**
+     * @covers AddonXmlGenerator::addSection
+     */
+    public function testAddSectionThrowsException(): void
+    {
+        $this->expectException(DuplicateIdException::class);
+        $xml_duplicate_sections =
+            $this
+                ->generator
+                ->createAddon()
+                ->setSettings()
+                ->addSection('defaults')
+                ->addSection('secondary')
+                ->addSection('defaults')
+                ->toString();
+    }
+
+    /**
+     * @covers AddonXmlGenerator::addSection
+     * @covers AddonXmlGenerator::setSetting
+     */
+    public function testSetSetting(): void
+    {
+        $expected_xml = <<<EOD
+        <addon>
+            <settings>
+                <sections>
+                    <section id="defaults">
+                        <items>
+                            <item id="vendor_name">
+                                <type>input</type>
+                                <default_value>shemenyov</default_value>
+                            </item>
+                            <item id="counter">
+                                <type>checkbox</type>
+                                <default_value/>
+                            </item>
+                        </items>
+                    </section>
+                    <section id="secondary">
+                        <items>
+                            <item id="number_of_emails">
+                                <type>selectbox</type>
+                                <variants>
+                                    <item id="one"></item>
+                                    <item id="three"></item>
+                                    <item id="ten"></item>
+                                </variants>
+                                <default_value>ten</default_value>
+                            </item>
+                        </items>
+                    </section>
+                </sections>
+            </settings>
+        </addon>
+EOD;
+        $xml =
+            $this
+                ->generator
+                ->createAddon()
+                ->setSettings()
+                ->addSection('defaults')
+                ->addSection('secondary')
+                ->setSetting(
+                    'defaults',
+                    'input',
+                    'vendor_name',
+                    'petrov'
+                )
+                ->setSetting(
+                    'defaults',
+                    'input',
+                    'vendor_name',
+                    'sergeev'
+                )
+                ->setSetting(
+                    'defaults',
+                    'input',
+                    'vendor_name',
+                    'shemenyov'
+                )
+                ->setSetting(
+                    'defaults',
+                    'checkbox',
+                    'counter'
+                )
+                ->setSetting(
+                    'secondary',
+                    'number_of_emails',
+                    'selectbox',
+                    'ten',
+                    [
+                        'one', 'three', 'ten'
+                    ]
+                )
+                ->toString();
+
+        $actual = new DOMDocument;
+        $actual->loadXML($xml);
+
+        $expected = new DOMDocument;
+        $expected->loadXML($expected_xml);
+
+        $this->assertEqualXMLStructure(
+            $expected->firstChild,
+            $actual->firstChild,
+            true
+        );
+    }
+
+    /**
+     * @covers AddonXmlGenerator::addSetting
+     */
+    public function testAddSetting(): void
+    {
+        $this->expectException(DuplicateIdException::class);
+        $xml_duplicate_sections =
+            $this
+                ->generator
+                ->createAddon()
+                ->setSettings()
+                ->addSection('defaults')
+                ->addSection('secondary')
+                ->addSection('defaults')
+                ->addSetting(
+                    'defaults',
+                    'input',
+                    'vendor_name',
+                    'shemenyov'
+                )
+                ->addSetting(
+                    'defaults',
+                    'input',
+                    'vendor_name',
+                    'shemenyov'
+                );
+    }
+
+    private function _testAddonNode(string $xml, string $name, string $value): void
+    {
+        $actual = new DOMDocument;
+        $actual->loadXML($xml);
+
+        $expected = new DOMDocument;
+        $expected->loadXML("<addon><$name>$value</$name></addon>");
+
+        $this->assertEqualXMLStructure(
+            $expected->firstChild,
+            $actual->firstChild,
+            true
+        );
+    }
+
+    private function _testAddonAttribute(string $xml, string $name, string $value): void
+    {
+        $actual = new DOMDocument;
+        $actual->loadXML($xml);
+
+        $expected = new DOMDocument;
+        $expected->loadXML("<addon $name=\"$value\"></addon>");
+
+        $this->assertEqualXMLStructure(
+            $expected->firstChild,
+            $actual->firstChild,
+            true
+        );
+    }
+}
---
+++ tests/generators/LanguageGeneratorTest.php
@@ -0,0 +1,422 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+use generators\Language\LanguageGenerator;
+use generators\Language\enums\LangvarTypes;
+
+final class LanguageGeneratorTest extends TestCase
+{
+    private $generator;
+    private $config;
+    private $testFilename = ROOT_DIR . '/tests/sources/po/test.po';
+
+    protected function setUp(): void
+    {
+        $this->config = new Config([
+            'addon.id=sd_addon'
+        ],
+        [
+            'addon.default_language' => 'en',
+            'filesystem.output_path_relative' => './'
+        ]);
+
+        $this->generator = new LanguageGenerator($this->config);
+    }
+
+    /**
+     * @covers LanguageGenerator::setEndingNewLine
+     */
+    public function testNewLine(): void
+    {
+        $without_new_line = <<<EOD
+check file
+for new
+line
+endings
+EOD;
+        $with_new_line = $without_new_line . PHP_EOL;
+        $this->assertEquals(
+            explode_by_new_line($with_new_line),
+            explode_by_new_line(LanguageGenerator::setEndingNewLine($without_new_line))
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::setContent
+     * @covers LanguageGenerator::toString
+     */
+    public function testSetContent(): void
+    {
+        $test_content = <<<EOD
+msgctxt "Languages::email_marketing.subscription_confirmed"
+msgid "Thank you for subscribing to our newsletter"
+msgstr "Merci de votre inscription à notre newsletter"
+
+EOD;
+
+        $this->assertEquals(
+            explode_by_new_line($test_content),
+            explode_by_new_line($this->generator->setContent($test_content)->toString())
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::setContent
+     * @covers LanguageGenerator::appendContent
+     * @covers LanguageGenerator::toString
+     */
+    public function testAppendContent(): void
+    {
+        $test_content = <<<EOD
+msgctxt "Languages::email_marketing.subscription_confirmed"
+msgid "Thank you for subscribing to our newsletter"
+msgstr "Merci de votre inscription à notre newsletter"
+
+msgctxt "Languages::tt_views_block_manager_update_block_width"
+msgid "Block width works for blocks located in the group having horizontal direction, for other cases this parameter does not work."
+msgstr "Block width works for blocks located in the group having horizontal direction, for other cases this parameter does not work."
+
+EOD;
+
+        $initial_content = <<<EOD
+msgctxt "Languages::email_marketing.subscription_confirmed"
+msgid "Thank you for subscribing to our newsletter"
+msgstr "Merci de votre inscription à notre newsletter"
+EOD;
+        $appending_content = <<<EOD
+msgctxt "Languages::tt_views_block_manager_update_block_width"
+msgid "Block width works for blocks located in the group having horizontal direction, for other cases this parameter does not work."
+msgstr "Block width works for blocks located in the group having horizontal direction, for other cases this parameter does not work."
+EOD;
+
+        $this->assertEquals(
+            explode_by_new_line($test_content),
+            explode_by_new_line(
+                $this->generator
+                    ->setContent($initial_content)
+                    ->appendContent($appending_content)
+                    ->toString()
+                )
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::getTranslationKey
+     */
+    public function testGetTranslationKey(): void
+    {
+        $this->assertSame(
+            LangvarTypes::$LANGUAGES . '::email_marketing.subscription_confirmed',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$LANGUAGES,
+                'email_marketing',
+                'subscription_confirmed'
+            )
+        );
+
+        $this->assertSame(
+            LangvarTypes::$SETTINGS_OPTIONS . '::special_jobs::special_request_title',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$SETTINGS_OPTIONS,
+                'special_jobs',
+                'special_request_title'
+            )
+        );
+
+        $this->assertSame(
+            LangvarTypes::$LANGUAGES . '::payments.epdq.tbl_bgcolor',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$LANGUAGES,
+                'payments.epdq',
+                'tbl_bgcolor'
+            )
+        );
+
+        $this->assertSame(
+            LangvarTypes::$SETTINGS_SECTIONS . '::sample_addon_3_0::section1',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$SETTINGS_SECTIONS,
+                'sample_addon_3_0',
+                'section1'
+            )
+        );
+
+        $this->assertSame(
+            LangvarTypes::$SETTINGS_TOOLTIPS . '::sample_addon_3_0::input',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$SETTINGS_TOOLTIPS,
+                'sample_addon_3_0',
+                'input'
+            )
+        );
+
+        $this->assertSame(
+            LangvarTypes::$SETTINGS_VARIANTS . '::sample_addon_3_0::radiogroup::radio_2',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$SETTINGS_VARIANTS,
+                'sample_addon_3_0',
+                'radiogroup',
+                'radio_2'
+            )
+        );
+
+        $this->assertSame(
+            LangvarTypes::$PROFILE_FIELDS . '::email',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$PROFILE_FIELDS,
+                '',
+                'email'
+            )
+        );
+
+        $this->assertSame(
+            LangvarTypes::$ADDONS . '::name::sample_addon_3_0',
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$ADDONS,
+                'name',
+                'sample_addon_3_0'
+            )
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::findByKey
+     */
+    public function testFindByKey(): void
+    {
+        $test_content = file_get_contents($this->testFilename);
+
+        $generator = new LanguageGenerator($this->config);
+
+        $this->assertSame(
+            [
+                'msgctxt'   => 'Languages::payments.epdq.tbl_bgcolor',
+                'msgid'     => 'Table background color',
+                'msgstr'    => 'Table background color',
+            ],
+            $generator->setContent($test_content)->findByKey(
+                LanguageGenerator::getTranslationKey(LangvarTypes::$LANGUAGES, 'payments.epdq', 'tbl_bgcolor')
+            )
+        );
+
+        $this->assertSame(
+            false,
+            $generator->findByKey(
+                LanguageGenerator::getTranslationKey(LangvarTypes::$LANGUAGES, 'not_found', 'test_cannot_find')
+            )
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::removeByKey
+     * @covers LanguageGenerator::setContent
+     * @covers LanguageGenerator::getTranslationKey
+     */
+    public function testRemove(): void
+    {
+        $test_content = <<<EOD
+msgctxt "Languages::tts_generate_submenu"
+msgid "The submenu will include child elements of the selected object."
+msgstr "The submenu will include child elements of the selected object."
+EOD;
+        $generator = new LanguageGenerator($this->config);
+
+        $generator->setContent($test_content)->removeByKey(
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$LANGUAGES,
+                '',
+                'tts_generate_submenu'
+            )
+        );
+
+        $this->assertSame(
+            '',
+            $generator->toString()
+        );
+
+        unset($generator);
+        unset($test_content);
+
+        // new test
+        $test_content = 'msgctxt "Languages::tts_generate_submenu"'
+            . "\r\n" . 'msgid "The submenu will include child elements of the selected object."'
+            . "\r\n" . 'msgstr "The submenu will include child elements of the selected object."';
+
+        $generator = new LanguageGenerator($this->config);
+
+        $generator->setContent($test_content)->removeByKey(
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$LANGUAGES,
+                '',
+                'tts_generate_submenu'
+            )
+        );
+
+        $this->assertSame(
+            '',
+            $generator->toString()
+        );
+
+        unset($generator);
+        unset($test_content);
+
+        // new test
+        $test_content = file_get_contents($this->testFilename);
+        $generator = new LanguageGenerator($this->config);
+
+        $generator->setContent($test_content);
+
+        // exists before removing
+        $this->assertSame(
+            [
+                'msgctxt'   => 'ProfileFields::email',
+                'msgid'     => 'E-mail',
+                'msgstr'    => 'E-mail',
+            ],
+            $generator->findByKey(
+                LanguageGenerator::getTranslationKey(
+                    LangvarTypes::$PROFILE_FIELDS,
+                    '',
+                    'email',
+                )
+            )
+        );
+
+        // removing
+        $generator->removeByKey(
+            LanguageGenerator::getTranslationKey(
+                LangvarTypes::$PROFILE_FIELDS,
+                '',
+                'email'
+            )
+        );
+
+        // not exists after removing
+        $this->assertSame(
+            false,
+            $generator->findByKey(
+                LanguageGenerator::getTranslationKey(
+                    LangvarTypes::$PROFILE_FIELDS,
+                    '',
+                    'email'
+                )
+            )
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::create
+     * @covers LanguageGenerator::toString
+     */
+    public function testCreate(): void
+    {
+        $heading = <<<EOD
+msgid ""
+msgstr ""
+"Language: en\\n"
+"Content-Type: text/plain; charset=UTF-8\\n"
+"Pack-Name: English\\n"
+"Lang-Code: en\\n"
+"Country-Code: US\\n"
+
+EOD;
+        $generator = new LanguageGenerator($this->config);
+
+        $this->assertEquals(
+            explode_by_new_line($heading),
+            explode_by_new_line($generator->create()->toString())
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::replaceLangvar
+     * @covers LanguageGenerator::toString
+     * @covers LanguageGenerator::getTranslationKey
+     */
+    public function testReplaceLangvar()
+    {
+        $generator = new LanguageGenerator($this->config);
+        $test_content = <<<EOD
+msgctxt "Languages::tts_generate_submenu"
+msgid "The submenu will include child elements of the selected object"
+msgstr "The submenu will include child elements of the selected object"
+
+EOD;
+
+        $generator->replaceLangvar(
+            LanguageGenerator::getTranslationKey(LangvarTypes::$LANGUAGES, '', 'tts_generate_submenu'),
+            'The submenu will include child elements of the selected object',
+            'The submenu will include child elements of the selected object'
+        );
+
+        $generator->replaceLangvar(
+            LanguageGenerator::getTranslationKey(LangvarTypes::$LANGUAGES, '', 'tts_generate_submenu'),
+            'The submenu will include child elements of the selected object',
+            'The submenu will include child elements of the selected object'
+        );
+
+        $this->assertEquals(
+            explode_by_new_line($test_content),
+            explode_by_new_line($generator->toString())
+        );
+
+        unset($generator);
+        unset($test_content);
+
+        $test_content = file_get_contents($this->testFilename);
+
+        $generator = new LanguageGenerator($this->config);
+        $generator->setContent($test_content);
+
+        $generator->replaceLangvar(
+            LanguageGenerator::getTranslationKey(LangvarTypes::$LANGUAGES, 'sd_miltomil', 'not_confirmed'),
+            'Your email already confirmed or confirmation code is wrong!'
+        );
+
+        $this->assertSame(
+            [
+                'msgctxt'   => 'Languages::sd_miltomil.not_confirmed',
+                'msgid'     => 'Your email already confirmed or confirmation code is wrong!',
+                'msgstr'    => 'Your email already confirmed or confirmation code is wrong!',
+            ],
+            $generator->findByKey(
+                LanguageGenerator::getTranslationKey(LangvarTypes::$LANGUAGES, 'sd_miltomil', 'not_confirmed')
+            )
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::checkLanguageSupport
+     */
+    public function testCheckLanguageSupport(): void
+    {
+        $this->assertSame(
+            true,
+            LanguageGenerator::checkLanguageSupport('en')
+        );
+
+        $this->assertSame(
+            true,
+            LanguageGenerator::checkLanguageSupport('ru')
+        );
+
+        $this->assertSame(
+            false,
+            LanguageGenerator::checkLanguageSupport('aa')
+        );
+    }
+
+    /**
+     * @covers LanguageGenerator::getPath
+     */
+    public function testGetPath(): void
+    {
+        $this->assertSame(
+            get_absolute_path(ROOT_DIR . $this->config->get('filesystem.output_path_relative') . 'var/langs/en/addons/sd_addon.po'),
+            $this->generator->getPath()
+        );
+    }
+}
